5a34d150a588a37d6679a22e7b6e45b0931d5908
vifattahi <vifattahi@gmail.com>
2023-01-04 16:09:33 +0330

    config ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import {currentUserRouter} from "./routes/current-user";
+import {loginUserRouter} from "./routes/login";
+import {logoutUserRouter} from "./routes/logout";
+import {registertUserRouter} from "./routes/register";
 
 const app = express();
 app.use(json());
 
 app.use('/api/users', currentUserRouter);
+app.use('/api/users', loginUserRouter);
+app.use('/api/users', logoutUserRouter);
+app.use('/api/users', registertUserRouter);
 
 app.listen(3000, () => {
     console.info('Listening on port 3000...');
