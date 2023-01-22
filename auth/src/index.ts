17e63f68b44b15420a503a7570d1612638fac3c4
vifattahi <vifattahi@gmail.com>
2023-01-22 17:24:26 +0330

    add mongo depl

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
 import { json } from 'body-parser';
 import {currentUserRouter} from "./routes/current-user";
 import {loginUserRouter} from "./routes/login";
@@ -9,12 +11,12 @@ import {NotFoundError} from "./errors/notFoudError";
 
 const app = express();
 app.use(json());
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use('/api/user', currentUserRouter);
 app.use('/api/user', loginUserRouter);
 app.use('/api/user', logoutUserRouter);
 app.use('/api/user', registerUserRouter);
-app.get('*', () => {
+app.all('*', async (req, res) => {
     throw new NotFoundError();
 })
 app.use(errorHandlers);
