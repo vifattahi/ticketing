d6cbaaa873afbdf3754698e5f2e6b4c9928a1377
vifattahi <vifattahi@gmail.com>
2023-01-29 17:13:25 +0330

    add jwt-secret env to k8s and modified register

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -29,6 +29,9 @@ app.all('*', async (req, res) => {
 app.use(errorHandlers);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('database was connected.')
--- auth/src/routes/register.ts
+++ auth/src/routes/register.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import {body, validationResult} from "express-validator";
+import jwt from 'jsonwebtoken';
 import {RequestValidationError} from "../errors/request-validation-error";
 
 import {User} from "../models/user";
@@ -29,6 +30,14 @@ router.post('/register',
         }
         const user = await User.build({ email, password });
         await user.save();
+        const userJWT = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!
+        );
+        req.session = {
+            jwt: userJWT
+        };
         res.status(201).send(user);
     });
 
