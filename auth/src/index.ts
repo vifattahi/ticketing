f2c68f59bfd83def4e0b706c98269c34008ca6b4
vifattahi <vifattahi@gmail.com>
2023-02-13 17:22:06 +0330

    refactor for test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,32 +1,6 @@
-import express from 'express';
 import 'express-async-errors';
-import morgan from 'morgan';
-import { json } from 'body-parser';
 import * as mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import {currentUserRouter} from "./routes/current-user";
-import {loginUserRouter} from "./routes/login";
-import {logoutUserRouter} from "./routes/logout";
-import {registerUserRouter} from "./routes/register";
-import {errorHandlers} from "./middlewares/error-handlers";
-import {NotFoundError} from "./errors/notFoudError";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(morgan('dev'));
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}));
-app.use('/api/user', currentUserRouter);
-app.use('/api/user', loginUserRouter);
-app.use('/api/user', logoutUserRouter);
-app.use('/api/user', registerUserRouter);
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-})
-app.use(errorHandlers);
+import {app} from "./app";
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -43,4 +17,3 @@ const start = async () => {
     })
 }
 start();
-
