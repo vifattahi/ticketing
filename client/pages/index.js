d20c306c4bf8b608be124f557e8e69dc692b5af2
vifattahi <vifattahi@gmail.com>
2023-03-15 12:03:02 +0330

    add register form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
     return <h1>This is a landing page!</h1>
 }
+
+LandingPage.getInitialProps = async () => {
+    const response = await axios.get('api/user/currentuser');
+    return response.data;
+}
+
+export default LandingPage;
